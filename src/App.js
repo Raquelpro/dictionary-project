b568a80d7a1226b19903dccb0628f6b33e6b6407
Raquelpro <107865288+Raquelpro@users.noreply.github.com>
2022-10-26 16:12:31 -0500

    Added button to project

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ export default function App() {
       <header className="App-header">
       <img src={logo} className="App-logo img-fluid"
       alt="logo" />
+      <br />
+      <a href="#" className="btn btn-primary shadow">Hello</a>
       </header>
     </div>
   );
