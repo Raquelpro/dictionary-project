fbaea7d4633874e2e029e2ead4bad52ed8235dfc
Raquelpro <107865288+Raquelpro@users.noreply.github.com>
2022-10-29 13:07:28 -0500

    Added api documentation URL

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -13,6 +13,7 @@ function handleResponse(response) {
 function search(event) {
     event.preventDefault();
 
+// documentation: https://dictionary.dev/
 let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
 axios.get(apiUrl).then(handleResponse);
 }
